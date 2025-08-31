23d04c1d4b0fbae24b746e43584c8d34344ab27e
jgaribaldi <julita.gari@gmail.com>
2025-08-31 17:54:16 -0300

    Replace pickle file with ChromaDB

--- context.py
+++ context.py
@@ -1,12 +1,9 @@
 import json
-import os
-import pickle
-from pathlib import Path
 from typing import List, Optional
 
-import numpy as np
+import chromadb
 import pandas
-from sentence_transformers import SentenceTransformer
+from chromadb.utils.batch_utils import create_batches
 from torch import Tensor
 
 
@@ -23,38 +20,35 @@ class RecipyData:
         self.steps = steps
 
 
-class RecipyContext:
-    def __init__(self) -> None:
-        self._model = SentenceTransformer("all-MiniLM-L6-v2")
-        self._loaded_embeddings = DataToSave(embeddings=[], texts=[])
+class RecipyStorage:
+    def __init__(self, file_path: str):
+        self._client = chromadb.PersistentClient(path=file_path)
+        self._collection = self._client.get_or_create_collection(
+            name="recipies",
+            metadata={
+                "description": "set of recipies in Spanish",
+            },
+        )
 
-    def find_similar(self, query: str, top_k: int = 1) -> Optional[List[str]]:
-        similarities = self._calculate_similarities(query)
-        top_indices = np.argsort(similarities)[-top_k:][::-1]
-        return [self._loaded_embeddings.texts[i] for i in top_indices]
+    def find_similar(self, query: str, top_k: int) -> Optional[List[str]]:
+        results = self._collection.query(query_texts=[query], n_results=top_k)
+        if results:
+            documents = results["documents"]
+            if documents:
+                return documents[0]
+        return None
 
-    def load_or_create_embeddings(
-        self, embeddings_file: str, raw_data_file: str
-    ) -> None:
-        file = Path(embeddings_file)
+    def add_data(self, raw_data_file: str) -> None:
+        self._create_embeddings(raw_data_file)
 
-        if not file.exists():
-            self._create_embeddings(raw_data_file, embeddings_file)
-        else:
-            file_empty = os.stat(embeddings_file).st_size == 0
-            if file_empty:
-                self._create_embeddings(raw_data_file, embeddings_file)
-
-        self._loaded_embeddings = pandas.read_pickle(embeddings_file)
-
-    def _create_embeddings(self, raw_data_file: str, embeddings_file: str) -> None:
+    def _create_embeddings(self, raw_data_file: str) -> None:
         print("Creating embeddings from raw data...")
         input_df = pandas.read_csv(raw_data_file, encoding="utf-8")
-
-        texts_to_embed = input_df["Ingredientes"].astype(str).to_list()
-        embeddings = [self._model.encode(text) for text in texts_to_embed]
-
-        texts_to_save = input_df.apply(
+        ids = input_df.apply(
+            lambda row: str(row["Id"]),
+            axis=1,
+        ).to_list()
+        documents = input_df.apply(
             lambda row: _row_to_json(
                 row["Nombre"],
                 row["Ingredientes"],
@@ -62,17 +56,16 @@ class RecipyContext:
             ),
             axis=1,
         ).to_list()
-
-        data_to_save = DataToSave(embeddings, texts_to_save)
-        with open(embeddings_file, "wb") as f:
-            pickle.dump(data_to_save, f)
-        print("Embeddings ready to use!")
-
-    def _calculate_similarities(self, query: str) -> List[float]:
-        query_embedding = self._model.encode(query)
-        query_array = np.array(query_embedding)
-        loaded_embeddings_array = np.array(self._loaded_embeddings.embeddings)
-        return np.dot(loaded_embeddings_array, query_array)
+        metadatas = input_df.apply(
+            lambda row: {"ingredients": row["Ingredientes"]},
+            axis=1,
+        ).to_list()
+        batches = create_batches(
+            api=self._client, ids=ids, documents=documents, metadatas=metadatas
+        )
+        for batch in batches:
+            print(f"Adding batch of size {len(batch[0])}")
+            self._collection.add(documents=batch[3], ids=batch[0], metadatas=batch[2])
 
 
 def _row_to_json(name: str, ingredients: str, steps: str) -> str:
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-from pprint import pprint
 from typing import List
 
-from context import RecipyContext, RecipyData, json_to_recipy_data
+from context import RecipyData, RecipyStorage, json_to_recipy_data
 from llm import Llm
 
 
 class CookieBotAgent:
-    def __init__(self, recipy_context: RecipyContext):
-        self._recipy_context = recipy_context
+    def __init__(self, recipy_storage: RecipyStorage):
+        self._recipy_storage = recipy_storage
 
     def process_request(self, request: str) -> List[str]:
         recipies = self._get_recipies(request)
         return [recipy.name for recipy in recipies]
 
     def _get_recipies(self, request: str) -> List[RecipyData]:
-        recipies = self._recipy_context.find_similar(request, 10)
+        recipies = self._recipy_storage.find_similar(request, 10)
         if recipies:
             return [json_to_recipy_data(recipy) for recipy in recipies]
         else:
@@ -24,19 +23,20 @@ class CookieBotAgent:
 if __name__ == "__main__":
     print("Hello CookieBot")
 
-    recipy_context = RecipyContext()
-    recipy_context.load_or_create_embeddings(
-        "data/embeddings.pickle", "data/recetasdelaabuela.csv"
-    )
-
     available_ingredients = ["huevos", "cebollas", "pescado"]
-    agent = CookieBotAgent(recipy_context)
-    recipies = agent.process_request(
-        f"Los ingredientes que tengo son: {", ".join(available_ingredients)}"
-    )
+    query = f"Los ingredientes que tengo son: {", ".join(available_ingredients)}"
 
-    llm = Llm(url="http://localhost:11434", sys_prompt_filename="system_prompt.txt")
-    selected_recipy = llm.ask_recipy(
-        ingredients=available_ingredients, recipies=recipies
-    )
-    print(selected_recipy)
+    recipy_storage = RecipyStorage(file_path="data/recipystorage.chroma")
+    # recipy_storage.add_data("data/recetasdelaabuela.csv")
+    # print("Finsihed adding data")
+    recipies = recipy_storage.find_similar(query, 10)
+
+    agent = CookieBotAgent(recipy_storage)
+    recipies = agent.process_request(query)
+
+    if recipies:
+        llm = Llm(url="http://localhost:11434", sys_prompt_filename="system_prompt.txt")
+        selected_recipy = llm.ask_recipy(
+            ingredients=available_ingredients, recipies=recipies
+        )
+        print(selected_recipy)
